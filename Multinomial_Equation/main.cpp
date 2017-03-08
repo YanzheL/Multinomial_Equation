0e3fac5a646777ab59b9f69892dea7c7e01fce30
YanzheL <lee.yanzhe@yanzhe.org>
2017-03-08 18:46:45 +0800

    Initial Commit

---
+++ Multinomial_Equation/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  Multinomial_Equation
+//
+//  Created by LI YANZHE on 08/03/2017.
+//  Copyright © 2017 Yanzhe Lee. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
